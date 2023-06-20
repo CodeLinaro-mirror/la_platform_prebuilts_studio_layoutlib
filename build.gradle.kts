1ffc0b707fb3e3cde181f60efcd31ef85ad616f4
Jerome Gaillard <jgaillard@google.com>
2023-06-20 17:51:05 +0100

    Create layoutlib artifacts for GMaven
    This creates a gradle project for building artifacts for layoutlib to be
    shipped on GMaven. One artifact is simply the layoutlib jar. Another one
    is composed of all the native libraries and associated data files
    (fonts, icu...). This latter artifact, named runtime, is using Gradle
    module metadata to only ship the relevant files for a given
    architecture/OS.
    
    Bug: 241795063
    Test: N/A
    Change-Id: I047218ec29a95bd5ec7def4cd30b4544aac47e30

---
+++ build.gradle.kts
@@ -0,0 +1,136 @@
+plugins {
+  `java-library`
+  `maven-publish`
+}
+
+version = "1.0.0"
+group = "com.android.tools.layoutlib"
+
+// Create task for a specific platform/architecture
+fun registerNativeTask(nativeFolder: String) {
+  tasks.register<Jar>(nativeFolder) {
+    archiveClassifier.set(nativeFolder)
+    from(layout.projectDirectory) {
+      include("data/$nativeFolder/**")
+      include("data/icu/**")
+      include("data/keyboards/**")
+      include("data/fonts/**")
+      include("licenses/icu/**")
+      if (nativeFolder.startsWith("mac")) {
+        include("licenses/mac/**")
+      } else {
+        include("licenses/$nativeFolder/**")
+      }
+      exclude("**/BUILD")
+    }
+  }
+}
+
+fun createNativeConfig(architecture: String, os: String): NamedDomainObjectContainerCreatingDelegateProvider<Configuration> {
+  return configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = false
+    attributes {
+      attribute(MachineArchitecture.ARCHITECTURE_ATTRIBUTE, project.objects.named(MachineArchitecture::class.java, architecture))
+      attribute(OperatingSystemFamily.OPERATING_SYSTEM_ATTRIBUTE, project.objects.named(OperatingSystemFamily::class.java, os))
+      attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage::class.java, Usage.JAVA_RUNTIME))
+    }
+  }
+}
+
+// Default task containing the native files for all platforms
+tasks.jar {
+  from(layout.projectDirectory) {
+    include("data/linux/**")
+    include("data/mac/**")
+    include("data/mac-arm/**")
+    include("data/win/**")
+    include("data/icu/**")
+    include("data/keyboards/**")
+    include("data/fonts/**")
+    include("licenses/**")
+    exclude("licenses/layoutlib.jar.txt")
+    exclude("**/BUILD")
+  }
+}
+
+registerNativeTask("linux")
+registerNativeTask("win")
+registerNativeTask("mac")
+registerNativeTask("mac-arm")
+
+val linux: Configuration by createNativeConfig(MachineArchitecture.X86_64, OperatingSystemFamily.LINUX)
+val windows: Configuration by createNativeConfig(MachineArchitecture.X86_64, OperatingSystemFamily.WINDOWS)
+val macX86: Configuration by createNativeConfig(MachineArchitecture.X86_64, OperatingSystemFamily.MACOS)
+@Suppress("UnstableApiUsage")
+val macArm: Configuration by createNativeConfig(MachineArchitecture.ARM64, OperatingSystemFamily.MACOS)
+
+artifacts.add("linux", tasks["linux"]) {}
+artifacts.add("windows", tasks["win"]) {}
+artifacts.add("macX86", tasks["mac"]) {}
+artifacts.add("macArm", tasks["mac-arm"]) {}
+
+val javaComponent = components.findByName("java") as AdhocComponentWithVariants
+javaComponent.addVariantsFromConfiguration(linux) {}
+javaComponent.addVariantsFromConfiguration(windows) {}
+javaComponent.addVariantsFromConfiguration(macX86) {}
+javaComponent.addVariantsFromConfiguration(macArm) {}
+
+publishing {
+  publications {
+    create<MavenPublication>("layoutlib-runtime") {
+      artifactId = "layoutlib-runtime"
+      from(components["java"])
+      pom {
+        name.set("Layoutlib runtime")
+        description.set("Native runtime for Layoutlib with its resources")
+        url.set("https://developer.android.com/studio")
+        licenses {
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+        developers {
+          developer {
+            name.set("The Android Open Source Project")
+          }
+        }
+        scm {
+          connection.set("scm:git:https://android.googlesource.com/platform/frameworks/base/")
+          url.set("https://cs.android.com/android/platform/frameworks/base/")
+        }
+      }
+    }
+    create<MavenPublication>("layoutlib") {
+      artifactId = "layoutlib"
+      artifact(file(layout.projectDirectory.file("data/layoutlib.jar")))
+      artifact(file(layout.projectDirectory.file("licenses/layoutlib.jar.txt")))
+      pom {
+        name.set("Layoutlib")
+        description.set("Rendering library for Android resources on non-Android platforms")
+        url.set("https://developer.android.com/studio")
+        licenses {
+          license {
+            name.set("The Apache License, Version 2.0")
+            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+          }
+        }
+        developers {
+          developer {
+            name.set("The Android Open Source Project")
+          }
+        }
+        scm {
+          connection.set("scm:git:https://android.googlesource.com/platform/frameworks/layoutlib/")
+          url.set("https://cs.android.com/android/platform/frameworks/layoutlib/")
+        }
+      }
+    }
+  }
+  repositories {
+    maven {
+      url = uri("${layout.buildDirectory.asFile.get()}/publishing-repository")
+    }
+  }
+}
